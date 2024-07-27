bf2db6d02a829bf68d2d646366d42cbd50e76b76
Joseph <adedejiosvaldo@gmail.com>
2024-07-27 15:52:53 +0100

    Feature -> Add resolver

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { typeDefs } from "./data/schema";
+import db from "./_db";
+const resolvers = {
+  Query: {
+    games() {
+      return db.games;
+    },
+    reviews() {
+      return db.reviews;
+    },
+    authors() {
+      return db.authors;
+    },
+  },
+};
 
 const server = new ApolloServer({
   typeDefs,
   //SCHEMA - describes the shape of the data that the server can send and receive
   //    Type def -> Def of different types we have on the GraphQL server
   //
+
+  resolvers,
 });
 
 const { url } = await startStandaloneServer(server, {
